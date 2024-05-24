16155b63b4086bb15a2bdbcfabdcd1a827d4cfd0
miguelbidarrab <miguelbidarrab.com>
2024-05-24 20:10:44 +0100

    Minor changes

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,3 +1,3 @@
 export default function ErrorPage() {
-    return <p>Sorry, something went wrong</p>
+    return <p>Sorry, something went wrong, mayb some pronblem with authenctication, wrong user, wrong password, short password, email not with @ something ...</p>
   }
--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -1,43 +1,50 @@
-import { createUser} from './createUser'
+import { createUser } from "./createUser";
+//import { createClient } from "@/utils/supabase/server";
 
-export default function Profiles() {
+export default async function Profiles() {
+/*   const supabase = createClient();
+  const { data: notes } = await supabase.from("profiles").select();
+ */
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <form className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg space-y-6">
-        <div className="form-control w-full">
-          <label htmlFor="email" className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div className="form-control w-full">
-          <label htmlFor="password" className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            required
-            className="input input-bordered w-full"
-          />
-        </div>
-        <div className="form-control">
-          <button
-            type="submit"
-            formAction={createUser}
-            className="btn btn-secondary w-full"
-          >
-            Create User
-          </button>
-        </div>
-      </form>
-    </div>
-  )
+    <>
+      <div className="flex items-center justify-center min-h-screen">
+        <form className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg space-y-6">
+          <div className="form-control w-full">
+            <label htmlFor="email" className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="form-control w-full">
+            <label htmlFor="password" className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              className="input input-bordered w-full"
+            />
+          </div>
+          <div className="form-control">
+            <button
+              type="submit"
+              formAction={createUser}
+              className="btn btn-secondary w-full"
+            >
+              Create User
+            </button>
+          </div>
+        </form>
+      </div>
+      {/* {JSON.stringify(notes, null, 2)} */}
+    </>
+  );
 }
